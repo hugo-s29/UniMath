7cf45d4fa73656117ee9e53f2d311dc3ae98f1ce
Hugo Salou <y.hugo.s29@gmail.com>
2026-09-10 13:56:50 +0200

    Colimits of module signatures

--- UniMath/CategoryTheory/Monoidal/ModuleSignatures.v
+++ UniMath/CategoryTheory/Monoidal/ModuleSignatures.v
@@ -22,10 +22,8 @@ Require Import UniMath.MoreFoundations.All.
 Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Functors.
 
-(*
 Require Import UniMath.CategoryTheory.Limits.Graphs.Limits.
 Require Import UniMath.CategoryTheory.Limits.Graphs.Colimits.
-*)
 
 Require Import UniMath.CategoryTheory.Monoidal.WhiskeredBifunctors.
 Require Import UniMath.CategoryTheory.Monoidal.Categories.
@@ -171,38 +169,329 @@ Section ModuleSignatures.
      3. Evaluation functor (for some fixed monoid R)
    *)
 
-  Context (R : MON C).
-  Let MOD_R := MOD (pr1 R) (pr2 R).
+  Section FixAMonoid.
+    Context (R : MON C).
+    Let MOD_R := MOD (pr1 R) (pr2 R).
 
 
-  Definition signature_evaluation_data
-    : functor_data module_signature_cat MOD_R.
+    Definition signature_evaluation_data
+      : functor_data module_signature_cat MOD_R.
+    Proof.
+      use make_functor_data.
+      - intro Σ; exact (Σ R).
+      - intros Σ Σ' [f _]; induction (f R) as [fR H].
+        exists fR.
+        abstract (
+          cbn in fR, H |- *;
+          unfold is_module_mor, pullback_functor_funct in *;
+          cbn in fR, H |- *;
+          etrans;
+          [|use H];
+          now rewrite assoc, tensor_mor_left, tensor_id_id, id_right
+        ).
+    Defined.
+
+    Lemma signature_evaluation_is_functor
+      : is_functor signature_evaluation_data.
+    Proof.
+      split.
+      - intro Σ. apply MOD_mor_eq.
+        easy.
+      - intros Σ Σ' Σ'' f g; apply MOD_mor_eq; cbn; unfold mor_disp; cbn.
+        now rewrite transportf_total2, transportf_const.
+    Qed.
+
+
+    Definition signature_evaluation : module_signature_cat ⟶ MOD_R
+      := make_functor signature_evaluation_data signature_evaluation_is_functor.
+  End FixAMonoid.
+
+
+  Section Colimits.
+    Context {g : graph}.
+    Context (d : diagram g module_signature_cat).
+    Context (colims_g : Colims_of_shape g C).
+    Context (HR : ∏ R : MON C, preserves_colimits_of_shape (rightwhiskering_functor C (pr1 R)) g).
+
+    Let Σ v : module_signature_data := dob d v.
+
+    Definition colimit_module_signature_diagram (R : MON C) 
+      : diagram g (MOD (pr1 R) (pr2 R))
+      := mapdiagram (signature_evaluation R) d.
+
+    Definition colimit_module_signature_colimcocone (R : MON C)
+      : ColimCocone (colimit_module_signature_diagram R)
+      := MOD_inherits_colimits _ _ _ colims_g (HR R) (colimit_module_signature_diagram R).
+
+    Definition colimit_module_signature_objects (R : MON C)
+      : MOD (pr1 R) (pr2 R)
+      := colim (colimit_module_signature_colimcocone R).
+
+    Let L R := colimit_module_signature_objects R.
+
+    Opaque MOD_inherits_colimits.
+
+    Definition colimit_module_signature_morphisms_cocone_data
+      (R R' : MON C) (f : R --> R') (v : vertex g)
+      : MOD (pr1 R) (pr2 R) ⟦ 
+          dob (colimit_module_signature_diagram R) v,
+          pullback_functor' R R' f (L R') ⟧.
+    Proof.
+      refine (_ · _).
+      - exact (section_disp_on_morphisms (Σ v) f).
+      - apply (# (pullback_functor' _ _ f)).
+        exact (colimIn (colimit_module_signature_colimcocone R') v).
+    Defined.
+
+    Lemma colimit_module_signature_morphisms_cocone_law
+      (R R' : MON C) (f : R --> R') 
+      : forms_cocone _ (colimit_module_signature_morphisms_cocone_data _ _ f).
+    Proof.
+      intros u v e; cbn.
+      use subtypePath.
+      { intro; use isaprop_is_module_mor. }
+      cbn; rewrite assoc; symmetry; etrans.
+      { apply cancel_precomposition.
+        use (maponpaths pr1 (!colimInCommutes (colimit_module_signature_colimcocone R') _ _ e)). }
+      cbn; rewrite assoc; use cancel_postcomposition.
+      etrans.
+      2: { exact (maponpaths pr1 (pr2 (dmor d e) _ _ f)). }  
+      unfold mor_disp; simpl.
+      rewrite transportf_total2; simpl.
+      now rewrite transportf_const.
+    Qed.
+
+    Definition colimit_module_signature_morphisms_cocone
+      (R R' : MON C) (f : R --> R')
+      : cocone (colimit_module_signature_diagram R) (pullback_functor' R R' f (L R')).
+    Proof.
+      use make_cocone.
+      - apply colimit_module_signature_morphisms_cocone_data.
+      - apply colimit_module_signature_morphisms_cocone_law.
+    Defined.
+
+    Definition colimit_module_signature_morphisms
+      (R R' : MON C) (f : R --> R')
+      : total_category_of_modules⟦(R ,, L R) , (R' ,, L R')⟧.
+    Proof.
+      exists f.
+      use (colimArrow _ (pullback_functor' _ _ _ _)).
+      use colimit_module_signature_morphisms_cocone.
+    Defined.
+
+    Definition colimit_module_signature_data : module_signature_data.
+    Proof.
+      use tpair.
+      - exact colimit_module_signature_objects.
+      - intros R R' f; apply colimit_module_signature_morphisms.
+    Defined.
+
+    Lemma colimit_module_signature_axioms
+      : section_disp_axioms colimit_module_signature_data.
+    Proof.
+      split.
+      - intro R; cbn.
+        symmetry.
+        use (colimArrowUnique _ _ (colimit_module_signature_morphisms_cocone _ _ (identity _))).
+        intro u; cbn; use subtypePath.
+        { intro. use isaprop_is_module_mor. }
+        cbn; symmetry.
+        rewrite id_right, <- id_left.
+        use cancel_postcomposition.
+        use (maponpaths pr1 (section_disp_id (dob d u) _)).
+      - intros R R' R'' f f'; cbn.
+        symmetry.
+        use (colimArrowUnique _ _ (colimit_module_signature_morphisms_cocone _ _ (f · f'))).
+        intro u; cbn; use subtypePath.
+        { intro. use isaprop_is_module_mor. }
+        cbn; rewrite assoc.
+        etrans.
+        { apply cancel_postcomposition.
+          apply (maponpaths pr1 (colimArrowCommutes (colimit_module_signature_colimcocone R)
+            _ (colimit_module_signature_morphisms_cocone _ _ _) _)). }
+        cbn; etrans.
+        { rewrite <- assoc; apply cancel_precomposition.
+          apply (maponpaths pr1 (colimArrowCommutes (colimit_module_signature_colimcocone R')
+            _ (colimit_module_signature_morphisms_cocone _ _ _) _)). }
+        cbn; rewrite assoc; use cancel_postcomposition.
+        use (maponpaths pr1 (!section_disp_comp (dob d u) _ _ _ _ _)).
+    Qed.
+
+    Definition colimit_module_signature : module_signature_cat
+      := colimit_module_signature_data ,,
+         colimit_module_signature_axioms.
+
+    Transparent MOD_inherits_colimits.
+
+    Definition colimit_module_signature_cocone
+      : cocone d colimit_module_signature.
+    Proof.
+      use make_cocone.
+      - intro v; use tpair.
+        + intro R; simpl.
+          exists (pr1 (colimIn (colimit_module_signature_colimcocone R) v)).
+          abstract (
+            unfold is_module_mor; cbn;
+            rewrite tensor_mor_left, tensor_id_id, id_left;
+            cbn; unfold colim_module_subst; cbn;
+            use (colimOfArrowsIn _ _ (ColimCocone_L_R _ _ _ _ (colimit_module_signature_diagram R)))
+          ).
+        + abstract (
+            intros R R' f; cbn;
+            use subtypePath; [intro; use isaprop_is_module_mor|];
+            unfold mor_disp; cbn;
+            rewrite transportf_total2; cbn;
+            rewrite transportf_const; cbn;
+            symmetry; use (colimArrowCommutes (colims_g (mapdiagram (forgetful _ _) (colimit_module_signature_diagram R))))
+          ).
+      - abstract (
+          intros u v e; cbn;
+          use subtypePath;
+          [intro; use isaprop_section_nat_trans_disp_axioms|];
+          cbn; use funextsec; intro R;
+          unfold mor_disp; cbn;
+          use subtypePath;
+          [intro; use isaprop_is_module_mor|];
+          rewrite transportf_total2; cbn;
+          rewrite transportf_const; cbn;
+          use (maponpaths pr1 (colimInCommutes (colimit_module_signature_colimcocone R) _ _ e))
+        ).
+    Defined.
+
+    Section FixACocone.
+      Context (Σ' : module_signature_cat) (cc : cocone d Σ').
+
+      Let cc' R := mapcocone (signature_evaluation R) _ cc.
+
+      Lemma colimit_module_signature_arrow_is_module_mor (R : MON C)
+        : is_module_mor _ _ 
+          (colim_module _ _ colims_g (HR _) _)
+          (pullback_functor_funct _ (pr2 (Σ' R)) _ (id_disp (pr2 R))) 
+          (pr1 (colimArrow (colimit_module_signature_colimcocone R) _ (cc' R))).
+      Proof.
+        unfold is_module_mor; cbn.
+        rewrite tensor_mor_left, tensor_id_id, id_left.
+        use (colimArrowUnique' (ColimCocone_L_R _ _ _ (HR R) (colimit_module_signature_diagram R))).
+        intro; do 2 rewrite assoc; etrans.
+        { apply cancel_postcomposition.
+          cbn; rewrite <- (bifunctor_rightcomp C).
+          apply maponpaths.
+          use (colimArrowCommutes (colims_g (mapdiagram (forgetful _ _) (colimit_module_signature_diagram R)))). }
+        unfold colim_module_subst; cbn.
+        symmetry; etrans.
+        { apply cancel_postcomposition.
+          use (colimOfArrowsIn _ _ (ColimCocone_L_R _ _ _ _ (colimit_module_signature_diagram R))). }
+        cbn; etrans.
+        { rewrite <- assoc; apply cancel_precomposition.
+          use (colimArrowCommutes (colims_g (mapdiagram (forgetful _ _) (colimit_module_signature_diagram R)))). }
+        cbn.
+        etrans.
+        { exact (!pr2 (pr1 (coconeIn cc u) R)). }
+        cbn.
+        now rewrite tensor_mor_left, tensor_id_id, id_left.
+      Qed.
+
+      Definition colimit_module_signature_arrow_data
+        : section_nat_trans_disp_data colimit_module_signature Σ'.
+      Proof.
+        intro R; eexists; use colimit_module_signature_arrow_is_module_mor.
+      Defined.
+
+      Lemma colimit_module_signature_arrow_nat
+        : section_nat_trans_disp_axioms colimit_module_signature_arrow_data.
+      Proof.
+        intros R R' f; cbn.
+        use subtypePath.
+        { intro; use isaprop_is_module_mor. }
+        unfold mor_disp; cbn.
+        rewrite transportf_total2; cbn.
+        rewrite transportf_const; cbn.
+        use colimArrowUnique'; intro; do 2 rewrite assoc.
+        etrans.
+        { apply cancel_postcomposition; use colimArrowCommutes. }
+        cbn; etrans.
+        { rewrite <- assoc; apply cancel_precomposition.
+          use (colimArrowCommutes (colims_g (mapdiagram (forgetful _ _) (colimit_module_signature_diagram R')))). }
+        cbn; symmetry; etrans.
+        { apply cancel_postcomposition.
+          use (colimArrowCommutes (colims_g (mapdiagram (forgetful _ _) (colimit_module_signature_diagram R)))). }
+        cbn.
+        etrans.
+        { exact (!maponpaths pr1 (pr2 (coconeIn cc u) _ _ f )). }
+        cbn.
+        unfold mor_disp; cbn.
+        rewrite transportf_total2; cbn.
+        now rewrite transportf_const.
+      Qed.
+
+      Definition colimit_module_signature_arrow 
+        : module_signature_cat ⟦ colimit_module_signature, Σ' ⟧.
+      Proof.
+        exists colimit_module_signature_arrow_data.
+        use colimit_module_signature_arrow_nat.
+      Defined.
+
+      Lemma colimit_module_signature_arrow_is_cocone_mor
+        : is_cocone_mor colimit_module_signature_cocone cc colimit_module_signature_arrow.
+      Proof.
+        intro; cbn.
+        use subtypePath.
+        { intro; use isaprop_section_nat_trans_disp_axioms. }
+        cbn; use funextsec; intro R.
+        use subtypePath.
+        { intro; use isaprop_is_module_mor. }
+        unfold mor_disp; cbn.
+        rewrite transportf_total2; cbn.
+        rewrite transportf_const; cbn.
+        use (colimArrowCommutes (colims_g (mapdiagram (forgetful _ _) (colimit_module_signature_diagram R)))).
+      Qed.
+
+      Context (pair : ∑ (γ : module_signature_cat ⟦ colimit_module_signature, Σ' ⟧),
+        is_cocone_mor colimit_module_signature_cocone cc γ).
+
+      Let γ : module_signature_cat ⟦ colimit_module_signature, Σ' ⟧ := pr1 pair.
+      Let γ_hyp : is_cocone_mor colimit_module_signature_cocone cc γ := pr2 pair.
+
+      Lemma colimit_module_signature_arrow_unique
+        : γ = colimit_module_signature_arrow.
+      Proof.
+        use subtypePath.
+        { intro; use isaprop_section_nat_trans_disp_axioms. }
+        use funextsec; intro R; cbn.
+        unfold colimit_module_signature_arrow_data; cbn.
+        use subtypePath.
+        { intro; use isaprop_is_module_mor. }
+        cbn.
+        use colimArrowUnique; intro u; cbn.
+        rewrite <- γ_hyp; cbn.
+        unfold mor_disp; cbn.
+        rewrite transportf_total2; cbn.
+        now rewrite transportf_const.
+      Qed.
+
+      Lemma colimit_module_signature_arrow_unique_pair
+        : pair = colimit_module_signature_arrow ,, colimit_module_signature_arrow_is_cocone_mor.
+      Proof.
+        use subtypePath.
+        - intro; use isaprop_is_cocone_mor.
+        - use colimit_module_signature_arrow_unique.
+      Qed.
+    End FixACocone.
+
+    Definition colimit_module_signature_ColimCocone : ColimCocone d.
+    Proof.
+      use make_ColimCocone.
+      - exact colimit_module_signature.
+      - exact colimit_module_signature_cocone.
+      - intros ? ?; eexists; use colimit_module_signature_arrow_unique_pair.
+    Defined.
+  End Colimits.
+
+  Theorem module_signature_inherits_limits (g : graph) (_ : Colims_of_shape g C)
+    (_ : ∏ R : MON C, preserves_colimits_of_shape (rightwhiskering_functor C (pr1 R)) g)
+    : Colims_of_shape g module_signature_cat.
   Proof.
-    use make_functor_data.
-    - intro Σ; exact (Σ R).
-    - intros Σ Σ' [f _]; induction (f R) as [fR H].
-      exists fR.
-      abstract (
-        cbn in fR, H |- *;
-        unfold is_module_mor, pullback_functor_funct in *;
-        cbn in fR, H |- *;
-        etrans;
-        [|use H];
-        now rewrite assoc, tensor_mor_left, tensor_id_id, id_right
-      ).
+    intro; now use colimit_module_signature_ColimCocone.
   Defined.
 
-  Lemma signature_evaluation_is_functor
-    : is_functor signature_evaluation_data.
-  Proof.
-    split.
-    - intro Σ. apply MOD_mor_eq.
-      easy.
-    - intros Σ Σ' Σ'' f g; apply MOD_mor_eq; cbn; unfold mor_disp; cbn.
-      now rewrite transportf_total2, transportf_const.
-  Qed.
-
-
-  Definition signature_evaluation : module_signature_cat ⟶ MOD_R
-    := make_functor signature_evaluation_data signature_evaluation_is_functor.
 End ModuleSignatures.
